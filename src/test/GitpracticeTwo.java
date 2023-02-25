602548b67f2dcee561a909f2fc98154557e155b5
meriemehabibena <mary.habibena@tekschool.us>
2023-02-25 17:49:31 -0500

    thsi is for push practice

--- src/test/GitpracticeTwo.java
+++ src/test/GitpracticeTwo.java
@@ -2,7 +2,13 @@ package test;
 
 public class GitpracticeTwo {
 	public static void main(String[]args) {
-	System.out.println("Thi is for push practice");
+	
+		
+		
+		System.out.println("Thi is for push practice");
+	
+	
+	
 
-}
-}
+
+}}
