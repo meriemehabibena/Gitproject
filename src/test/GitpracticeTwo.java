a1f7a8dd35709569e5b34948d940f2724c030aaf
meriemehabibena <125611314+meriemehabibena@users.noreply.github.com>
2023-02-25 17:57:13 -0500

    Update GitpracticeTwo.java
    This is for pull practice

--- src/test/GitpracticeTwo.java
+++ src/test/GitpracticeTwo.java
@@ -3,6 +3,7 @@ package test;
 public class GitpracticeTwo {
 	public static void main(String[]args) {
 	System.out.println("Thi is for push practice");
+	System.out.println("this is for pull practice";
 
 }
 }
