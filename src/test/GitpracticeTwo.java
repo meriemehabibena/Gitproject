5c2ea7f325e14490e9dce896b6514231e67f6b69
meriemehabibena <mary.habibena@tekschool.us>
2023-02-25 17:20:47 -0500

    new methode added and old one deleted

---
+++ src/test/GitpracticeTwo.java
@@ -0,0 +1,8 @@
+package test;
+
+public class GitpracticeTwo {
+	public static void main(String[]args) {
+	System.out.println("Thi is for push practice");
+
+}
+}
--- src/test/UnitTest.java
+++ src/test/UnitTest.java
@@ -4,7 +4,17 @@ public class UnitTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+	
+	
+		System.out.println(testone(10,5));
+	
+	}
+	public static int testone(int a, int b) {
+		
+		
+		return a-b;
+	}
 	}
 
-}
+
