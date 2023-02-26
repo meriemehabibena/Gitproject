15416e1f59c883ba00c62900afab15035feee776
meriemehabibena <mary.habibena@tekschool.us>
2023-02-25 20:10:13 -0500

    chnages in GitpracticeTwo

--- src/test/GitpracticeTwo.java
+++ src/test/GitpracticeTwo.java
@@ -3,12 +3,14 @@ package test;
 public class GitpracticeTwo {
 	public static void main(String[]args) {
 	
+		int a = 20;
+		int b = 20;
+		int c = a+b;
 		
 		
-		System.out.println("Thi is for push practice");
+		System.out.print(c);
 	
 	
 	
-
 
 }}
