f07312c901a8fda942bb728fdacb1edaf7d92d0a
meriemehabibena <mary.habibena@tekschool.us>
2023-02-26 09:14:12 -0500

    //this is a comment

--- src/test/GitpracticeTwo.java
+++ src/test/GitpracticeTwo.java
@@ -2,10 +2,11 @@ package test;
 
 public class GitpracticeTwo {
 	public static void main(String[]args) {
-	System.out.println("Thi is for push practice");
-	System.out.println("this is for pull practice";
-	System.out.println("this is second test pull practice")
-	System.out.println("this is second test pull practice")
+	System.out.println("This is for push practice");
+	System.out.println("this is for pull practice");
+	System.out.println("this is second test pull practice");
+	System.out.println("this is second test pull practice");
+//this is comment
 
 
 }
